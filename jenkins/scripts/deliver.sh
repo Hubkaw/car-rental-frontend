8950e284154f4f215bd1e3a78f6a58e0aa28695b
Hubkaw <hubkaw@gmail.com>
2025-08-31 17:25:55 +0200

    update deliver.sh again

--- jenkins/scripts/deliver.sh
+++ jenkins/scripts/deliver.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 
 npm run ng build
-npm run ng serve -- --host 0.0.0.0 --port 4200 &
+npm run ng serve -- --host 0.0.0.0 --port 4200 --disable-host-check &
 sleep 3
 echo $! > .pidfile
 
